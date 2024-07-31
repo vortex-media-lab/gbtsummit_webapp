d7bc63b3044449b3d62ab0f6896d8b1a2cb6040a
carlos <carlos.boor@gmail.com>
2024-07-31 00:46:21 -0600

    ajustes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 // next.config.mjs
 export default {
-  reactStrictMode: true,
-  images: {
-    domains: ["gbtsummit2024.com"],
-  },
-};
+    reactStrictMode: true,
+    images: {
+      domains: ['gbtsummit2024.com'],
+    },
+  };
+  
